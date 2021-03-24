884bfa3acae756d51b7e71db2ce117670c64f2d5
Timofey Sorokin <Ghost.Spartan36@outlook.com>
2021-03-24 12:28:18 +0300

    Задание 1

--- Task2/task2.py
+++ Task2/task2.py
@@ -1,15 +1,14 @@
+from collections import Counter
+
 print('Посещаемость')
 
+students_array = []
 num_of_lists = int(input('Количество списков: '))
-num_of_students = 0
-counter = 0
-list_array = []
-
 for i in range(num_of_lists):
     num_of_students = int(input('Количество фамилий в списке: '))
-    students_set = set()
     for j in range(num_of_students):
-        students_set.add(input('Введите имя ученика: '))
-    list_array.append(students_set)
-print(list_array)
-for j in list_array:
+        students_array.append(input('Введите имя ученика: '))
+dictionary =dict(Counter(students_array))
+for element in dictionary:
+    if dictionary.get(element) == num_of_lists:
+        print(element)
