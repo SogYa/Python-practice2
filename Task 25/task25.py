14469e51d13caac65f544bb20ff5af8332ad8072
Timofey Sorokin <Ghost.Spartan36@outlook.com>
2021-03-28 11:05:13 +0300

    Task 25

---
+++ Task 25/task25.py
@@ -0,0 +1,9 @@
+print('Сортировка в обратном порядке')
+
+nums_array= []
+for i in range(int(input('Введите количество чисел: '))):
+    nums_array.append(int(input('Введите число: ')))
+nums_array.sort()
+nums_array.reverse()
+for i in nums_array:
+    print(i)
